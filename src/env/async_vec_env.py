9a65b80d6e4fb6eca55af4e7fc80cc17a34943df
won-jun Kim <glisteirng96@gmail.com>
2023-05-03 22:59:58 +0900

    Experiment code on AsyncVecEnv library

---
+++ src/env/async_vec_env.py
@@ -0,0 +1,214 @@
+import os
+from typing import Callable
+
+import numpy as np
+
+from src.env.tsp_gymnasium import TSPEnv
+import gymnasium as gym
+import multiprocessing as mp
+import multiprocessing.dummy as mpd
+
+import logging
+import time
+
+logger = logging.getLogger("async_debug")
+logger.setLevel(logging.INFO)
+simple_formatter = logging.Formatter("[%(name)s] %(message)s")
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(simple_formatter)
+
+logger.addHandler(stream_handler)
+
+
+class AsyncVecEnv:
+    def __init__(self,
+                 env_fns: Callable[[gym.Env], TSPEnv],
+                 num_envs: int = 256,
+                 env_kwargs=None,
+                 num_processes:int = None,
+                 ):
+        self.num_processes = num_processes if not None else os.cpu_count()-2
+        self.env_fns = env_fns
+        self.num_envs = num_envs
+        self.env_kwargs = env_kwargs
+
+        self.envs = [env_fns(**env_kwargs) for _ in range(num_envs)]
+
+        self.mp_type = 'p'
+
+    def _reset(self, env_idx, seed=None):
+        obs, _ = self.envs[env_idx].reset(seed=seed)
+        return obs
+
+    def _step(self, env_idx, action):
+        try:
+            obs, reward, done, _, info = self.envs[env_idx].step(action)
+            return obs, reward, done, info
+
+        except:
+            print(self.envs[env_idx].available)
+            print(f"env_idx: {env_idx}, action: {action}")
+
+            raise Exception
+
+    def reset(self, seed=None):
+        if seed is not None:
+            to_do = [(i, seed+i) for i in range(self.num_envs)]
+
+        else:
+            to_do = [(i, None) for i in range(self.num_envs)]
+
+        if self.mp_type == 'p':
+            logger.debug(f"multiprocessing reset start")
+            st = time.time()
+
+            pool = mp.Pool(processes=self.num_processes)
+
+            results = pool.starmap(self._reset, to_do)
+
+            pool.close()
+            pool.join()
+
+            logger.debug(f"multiprocessing reset end, time: {time.time()-st}")
+
+        elif self.mp_type == 't':
+            logger.debug(f"threading reset start")
+            st = time.time()
+
+            pool = mpd.Pool(processes=self.num_processes)
+
+            results = pool.starmap(self._reset, to_do)
+
+            pool.close()
+            pool.join()
+
+            logger.debug(f"threading reset end, time: {time.time()-st}")
+
+        else:
+            logger.debug(f"Sequential reset start")
+            st = time.time()
+
+            results = [self._reset(*args) for args in to_do]
+
+            logger.debug(f"Sequential reset end, time: {time.time()-st}")
+
+        # stack the results into a single numpy array as obs variable
+        obs = {}
+        for key in results[0].keys():
+            obs[key] = np.stack([result[key] for result in results])
+
+        return obs
+
+    def step(self, action: np.ndarray):
+        # action: (num_envs, )
+        if self.mp_type == 'p':
+            logger.debug(f"multiprocessing step start")
+            st = time.time()
+
+            pool = mp.Pool(processes=self.num_processes)
+
+            results = pool.starmap(self._step, [(i, action[i]) for i in range(self.num_envs)])
+
+            pool.close()
+            pool.join()
+
+            logger.debug(f"multiprocessing step end, time: {time.time()-st}")
+
+        elif self.mp_type == 't':
+            logger.debug(f"threading step start")
+            st = time.time()
+
+            pool = mpd.Pool(processes=self.num_processes)
+
+            results = pool.starmap(self._step, [(i, action[i]) for i in range(self.num_envs)])
+
+            pool.close()
+            pool.join()
+
+            logger.debug(f"threading step end, time: {time.time()-st}")
+
+        else:
+            logger.debug(f"Sequential step start")
+            st = time.time()
+
+            results = [self._step(i, action[i]) for i in range(self.num_envs)]
+
+            logger.debug(f"Sequential step end, time: {time.time()-st}")
+
+        # results: [(obs, reward, done, debug), ...]
+        obs = {}
+
+        for key in results[0][0].keys():
+            obs[key] = np.stack([result[0][key] for result in results])
+
+        rewards = np.stack([result[1] for result in results])
+        dones = np.stack([result[2] for result in results])
+
+        return obs, rewards, dones, {}
+
+
+def run_exp(mp_type, env_params):
+    n_env = 2048
+    env = AsyncVecEnv(TSPEnv, env_kwargs=env_params, num_envs=n_env, num_processes=32)
+    env.mp_type = mp_type
+
+    logger.info(f"Exp using {mp_type}")
+
+    st = time.time()
+    env.reset(seed=None)
+
+    done = False
+
+    debug_action = 1
+
+    while not done:
+        action = np.array([debug_action for _ in range(n_env)])
+        obs, rewards, dones, debugs = env.step(action)
+
+        done = (dones == True).all()
+
+        debug_action += 1
+
+    logger.info(f"{mp_type} time: {time.time() - st}")
+
+
+
+if __name__ == '__main__':
+    env_params = {
+        'num_nodes': 10,
+        'num_depots': 1,
+        'seed': 1,
+        'step_reward': False,
+        'env_type': 'tsp',
+        'render_mode': None,
+
+    }
+
+    for mp_type in ['t', 's']:
+        run_exp(mp_type, env_params)
+
+    from gymnasium.vector import SyncVectorEnv
+
+    env = SyncVectorEnv([lambda: TSPEnv(**env_params) for _ in range(2048)])
+
+    logger.info(f"Exp using SyncVectorEnv")
+
+    st = time.time()
+    env.reset()
+
+    done = False
+
+    debug_action = 1
+
+    while not done:
+        action = np.array([debug_action for _ in range(2048)])
+        obs, rewards, dones, _, debugs = env.step(action)
+
+        done = (dones == True).all()
+
+        debug_action += 1
+
+    logger.info(f"SyncVectorEnv time: {time.time() - st}")
+
+
---
+++ src/env/tsp_gymnasium.py
@@ -0,0 +1,296 @@
+import os.path
+import pickle
+import time
+
+import gymnasium as gym
+import numpy as np
+import pygame as pygame
+from gymnasium.spaces import Discrete, Dict, Box, MultiBinary
+from gymnasium.utils import seeding
+
+from src.common.data_manipulator import make_cord, make_demands
+from src.common.utils import cal_distance
+
+
+class TSPEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 2}
+
+    def __init__(self, num_nodes,
+                 step_reward=False, render_mode=None, training=True, seed=None, data_path='./data', **kwargs):
+        super(TSPEnv, self).__init__()
+        self.action_size = num_nodes
+        self.num_nodes = num_nodes
+        self.step_reward = step_reward
+        self.training = training
+        self.seed = seed
+        self.data_path = data_path
+        self.env_type = 'tsp'
+
+        self.observation_space = Dict(
+            {
+                "xy": Box(0.0, 1.0, (self.action_size, 2), dtype=np.float32),
+                "pos": Discrete(self.action_size),
+                "available": MultiBinary(self.action_size)
+             }
+        )
+
+        self.action_space = Discrete(self.action_size, seed=seed)
+
+        # rendering fields
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        self.screen = None
+
+        # observation fields
+        self.xy, self.pos, self.visited = None, None, None
+        self.visiting_seq = None
+        self.available = None
+        self.t = 0
+
+        self.test_data_type = kwargs.get('test_data_type')
+        self._load_data_idx = 0
+
+    def seed(self, seed):
+        self._np_random, self._seed = seeding.np_random(seed)
+
+    def _get_obs(self):
+        return {"xy": self.xy, "pos": self.pos, "available": self.available, "t": self.t}
+
+    def _make_problems(self, num_rollouts, num_nodes):
+        xy = make_cord(num_rollouts, 0, num_nodes)
+
+        if num_rollouts == 1:
+            xy = xy.squeeze(0)
+
+        return xy
+
+    def _load_data(self, filepath):
+        # data format: (xy)
+        ext = filepath.split('.')[-1]
+
+        if ext == 'npz':
+            loaded_data = np.load(filepath)
+            xy = loaded_data['xy']
+
+        elif ext == 'pkl':
+            with open(filepath, 'rb') as f:
+                xy = pickle.load(f)
+
+            xy = np.array(xy, dtype=np.float32)[self._load_data_idx, :]
+            self._load_data_idx += 1
+
+        else:
+            raise ValueError(f"Invalid file extension for loading data: {ext}")
+        return xy
+
+    def _load_problem(self):
+        if self.test_data_type == 'npz':
+            file_path = f"{self.data_path}/tsp/N_{self.num_nodes}.npz"
+
+        else:
+            file_path = f"{self.data_path}/tsp/tsp{self.num_nodes}_test_seed1234.pkl"
+
+        if os.path.isfile(file_path):
+            xy = self._load_data(file_path)
+
+        else:
+            xy = make_cord(1, 0, self.num_nodes)
+
+            if not os.path.exists(self.data_path):
+                os.makedirs(self.data_path, exist_ok=True)
+
+            np.savez_compressed(file_path, xy=xy, demands=None)
+
+        if xy.ndim == 3:
+            xy = xy.reshape(-1, 2)
+
+        return xy
+
+    def _init_rendering(self):
+        if self.render_mode is not None:
+            # Set screen dimensions
+            self.screen_width = 900
+            self.screen_height = 600
+
+            # Set node size and edge width
+            self.node_size = 30
+            self.edge_width = 2
+
+            scaler = 450
+            left_margin = scaler * 0.28
+            top_margin = scaler * 0.2
+            bottom_margin = 100
+            right_margin = 100
+
+            # Define colors
+            self.WHITE = (255, 255, 255)
+            self.BLACK = (0, 0, 0)
+            self.RED = (255, 0, 0)
+            self.BLUE = (0, 0, 255)
+
+            self.scaled_xy = [(float(x * scaler) + left_margin, float(y * scaler) + top_margin) for x, y in self.xy]
+
+            pygame.init()
+            # Define font
+            self.display_font = pygame.font.Font(None, 30)
+            self.node_font = pygame.font.Font(None, 20)
+
+            # Create screen
+            self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), flags=pygame.HIDDEN)
+
+    def get_reward(self):
+        if self._is_done() or self.step_reward:
+            visitng_idx = np.array(self.visiting_seq, dtype=int)[None, :]
+            dist = cal_distance(self.xy[None, :], visitng_idx)
+            return -float(dist)
+
+        else:
+            return 0
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        if self.training:
+            self.xy = self._make_problems(1, self.num_nodes)
+
+        else:
+            self.xy = self._load_problem()
+
+        init_depot = 0
+        self.pos = init_depot
+        self.visited = np.zeros(self.action_size, dtype=bool)
+        self.visited[self.pos] = True
+        self.visiting_seq = []
+
+        self.visiting_seq.append(init_depot)
+        self.available = np.ones(self.action_size, dtype=bool)
+        self.available[init_depot] = False  # for the initial depot
+
+        self._init_rendering()
+        self.t = 0
+
+        obs = self._get_obs()
+
+        return obs, {}
+
+    def step(self, action):
+        # action: (1, )
+
+        assert action not in self.visiting_seq, f"visited nodes: {self.visiting_seq}, selected node: {action}"
+
+        # update the current pos
+        self.pos = action
+
+        # append the visited node idx
+        self.visiting_seq.append(action)
+
+        # update visited nodes
+        self.visited[action] = True
+
+        # assign avail to field
+        self.available, done = self.get_avail_mask()
+
+        reward = self.get_reward()
+
+        info = {}
+
+        self.t += 1
+
+        obs = self._get_obs()
+
+        return obs, reward, done, False, info
+
+    def _is_done(self):
+        done_flag = (self.visited[:] == True).all()
+        return bool(done_flag)
+
+    def get_avail_mask(self):
+        # get a copy of avail
+        avail = ~self.visited.copy()
+
+        done = self._is_done()
+
+        return avail, done
+
+    def render(self):
+        if self.render_mode is None:
+            return
+
+        assert self.screen is not None, "render mode setting is wrong"
+
+        canvas = pygame.Surface((self.screen_width, self.screen_height))
+        canvas.fill(self.WHITE)
+
+        # Draw edge
+        if len(self.visiting_seq) > 1:
+            current_node = self.visiting_seq[0]
+            prev_xy = self.scaled_xy[current_node]
+
+            for next_node in self.visiting_seq[1:]:
+                next_xy = self.scaled_xy[next_node]
+
+                # if current_node != next_node:
+                pygame.draw.line(canvas, self.BLACK, prev_xy, next_xy, self.edge_width)
+
+                prev_xy = next_xy
+
+        # Draw nodes and edges
+        for i, (x, y) in enumerate(self.scaled_xy):
+            if i == self.pos:
+                node_color = self.BLUE
+            elif i == 0:
+                node_color = self.RED
+            else:
+                node_color = self.BLACK
+
+            # Draw node
+            pygame.draw.circle(canvas, node_color, (x, y), self.node_size)
+
+            text_surface = self.node_font.render(f"{i}", True, self.WHITE)
+            text_rect = text_surface.get_rect(center=(x, y))
+            canvas.blit(text_surface, text_rect)
+
+        # Current distance cal
+        self.step_reward = True
+        reward = -self.get_reward()
+        self.step_reward = False
+
+        # current dist show
+        dist_text = self.display_font.render("Distance: {:.3f}".format(float(reward)), True, self.BLACK)
+        dist_rect = dist_text.get_rect(topright=(self.screen_width -  self.screen_width*0.1, self.screen_height*0.1))
+        canvas.blit(dist_text, dist_rect)
+
+        if self.render_mode == 'human':
+            # Update screen
+            self.screen.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+        else:
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1,0,2)
+            )
+
+    def close(self):
+        pygame.quit()
+
+    def set_test_mode(self):
+        self.training = False
+
+
+if __name__ == '__main__':
+    done = False
+    env = TSPEnv(10)
+    from src.env.VecEnv import RoutingVecEnv
+    from stable_baselines3.common.env_util import make_vec_env
+
+    env = make_vec_env(TSPEnv, n_envs=1, env_kwargs={'num_nodes': 10}, vec_env_cls=RoutingVecEnv)
+    obs = env.reset()
+    # mask = env.get_avail_mask()[0].astype(np.int8)
+
+    while not done:
+        action = np.array([env.action_space.sample()], dtype=int)
+        obs, reward, done, info = env.step(action)
+        # mask = env.get_avail_mask()[0].astype(np.int8)
+
+    print(reward)
