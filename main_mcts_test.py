e322ef739770a17d1a99b93606de76af016cfd40
won-jun Kim <glisteirng96@gmail.com>
2023-05-11 17:15:43 +0900

    modification) one full pipeline for mcts result test

--- main_mcts_test.py
+++ main_mcts_test.py
@@ -17,7 +17,7 @@ def run_test(**kwargs):
     #                                     ignore_debug=True, return_checkpoint=True)
     score, runtime = run_mcts_test(args)
     # the result json folder is the same as the model folder
-    return score, runtime
+    return score, runtime, args.test_data_idx
 
 
 def run_parallel_test(param_ranges, num_proc=4):
@@ -27,93 +27,73 @@ def run_parallel_test(param_ranges, num_proc=4):
 
     """
 
-    result = {}
-    # result: {'mcts_param_set_0': {'params': mcts_params, 'scores': {epoch: score, epoch: score, ...}, 'runtime': 10},
-    #          'mcts_param_set_1': {'params': mcts_params, 'scores': {epoch: score, epoch: score, ...}, 'runtime': 10},
-    save_path = None
-    runtime = 0
-    i = 0
+    result_dict = {}
     async_result = mp.Queue()
 
-    if not check_debug():
-        def __callback(val):
-            async_result.put(val)
+    def __callback(val):
+        async_result.put(val)
 
-        pool = mp.Pool(num_proc)
+    pool = mp.Pool(num_proc)
 
-        for params in dict_product(param_ranges):
-            pool.apply_async(run_test, kwds=params, callback=__callback)
+    for params in dict_product(param_ranges):
+        pool.apply_async(run_test, kwds=params, callback=__callback)
 
-        pool.close()
-        pool.join()
-
-    else:
-        for params in dict_product(param_ranges):
-            async_result.put(run_test(**params))
-
-    mcts_param_idx = 0
+    pool.close()
+    pool.join()
 
     while not async_result.empty():
-        score, time, k, save_path, mcts_params = async_result.get()
+        score, runtime, test_data_idx = async_result.get()
+        result_dict[test_data_idx] = {'score': score, 'runtime': runtime}
 
-        # if mcts params are not in the result dict, then it is a new set of mcts params
-        # so we need to create a new key for the result dict
+    avg_score = sum([result_dict[i]['score'] for i in range(num_problems)]) / num_problems
+    avg_runtime = sum([result_dict[i]['runtime'] for i in range(num_problems)]) / num_problems
 
-        assigned = False
+    std_score = sum([(result_dict[i]['score'] - avg_score) ** 2 for i in range(num_problems)]) / num_problems
+    std_runtime = sum([(result_dict[i]['runtime'] - avg_runtime) ** 2 for i in range(num_problems)]) / num_problems
 
-        # check if the mcts
-        for param_set_name, param_set in result.items():
-            if mcts_params in param_set.values():
-                # if mcts params are in the result dict, then we just need to add the score and runtime
-                param_set['score'][k] = score
-                param_set['runtime'] += time
-                assigned = True
-                break
+    result_dict['average'] = {'score': avg_score, 'runtime': avg_runtime}
+    result_dict['std'] = {'score': std_score, 'runtime': std_runtime}
 
-        if not assigned:
-            # if mcts params are not in the result dict, then it is a new set of mcts params
-            # so we need to create a new key for the result dict
-            result[f'mcts_param_set_{mcts_param_idx}'] = {}
-            result[f'mcts_param_set_{mcts_param_idx}']['params'] = mcts_params
-            result[f'mcts_param_set_{mcts_param_idx}']['score'] = {k: score}
-            result[f'mcts_param_set_{mcts_param_idx}']['runtime'] = time
-            mcts_param_idx += 1
+    if not Path(f"./result_summary/").exists():
+        Path(f"./result_summary/").mkdir(parents=True, exist_ok=False)
 
-    assert save_path is not None, "Wrong in fetching save path"
-    # save path is the same as the model path
+    # write the result_dict to a json file
+    with open(f"./result_summary/"
+              f"{run_param_dict['env_type'][0]}_{problem_size}_{run_param_dict['load_epoch'][0]}_test_result.json", 'w') as f:
+        json.dump(result_dict, f, indent=4)
 
-    # calculate the average runtime for each mcts param set
-    for param_set_name, param_set in result.items():
-        runtime = param_set['runtime']
-        i = len(param_set['score'])
-        param_set['runtime'] = runtime / i
-
-    if not Path(save_path).exists():
-        Path(save_path).mkdir(parents=True, exist_ok=False)
-
-    filename = "test_result"
-    file_dir = f'{save_path}/{filename}.json'
-
-    num_iter = 0
-
-    while Path(file_dir).exists():
-        filename = f'{filename}_{num_iter}'
-        file_dir = f'{save_path}/{filename}.json'
-        num_iter += 1
-
-    with open(file_dir, "w") as f:
-        json.dump(result, f, indent=2)
 
 
 if __name__ == '__main__':
-    param_dict = {
-        "num_nodes": [20],
-        "num_simulations": [40],
-        "cpuct": [1.1, 2],
-        'epochs': [495000, 500000],
+    problem_size = 50
+    num_problems = 100
+
+    run_param_dict = {
+        'test_data_type': ['pkl'],
         'env_type': ['tsp'],
-        'encoder_layer_num': [2],
-        'render_mode': [None]
+        'num_nodes': [problem_size],
+        'num_episode': [1024],
+        'test_data_idx': list(range(num_problems)),
+        'num_simulations': [problem_size*2]
     }
-    run_test(**{'load_epoch': 40000, 'test_data_type': 'pkl' , 'env_type': 'tsp', 'num_episode': 1024})
-    # run_parallel_test(param_dict, num_proc=1)
+
+    # result_dict = {}
+    #
+    # for i in range(num_problems):
+    #     run_param_dict['test_data_idx'] = i
+    #     score, runtime, i = run_test(**run_param_dict)
+    #
+    #     result_dict[i] = {'score': score, 'runtime': runtime}
+    #
+    # if not Path(f"./result_summary/").exists():
+    #     Path(f"./result_summary/").mkdir(parents=True, exist_ok=False)
+    #
+    # # write the result_dict to a json file
+    # with open(f"./result_summary/{run_param_dict['env_type']}_{problem_size}_test_result.json", 'w') as f:
+    #     json.dump(result_dict, f, indent=4)
+    #
+
+    for load_epoch in list(range(250000, 500000)) + ['best']:
+        run_param_dict['load_epoch'] = [load_epoch]
+        run_parallel_test(run_param_dict, num_proc=4)
+
