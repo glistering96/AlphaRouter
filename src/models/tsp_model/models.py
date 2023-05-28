8684b1761ad463dcf73e747dd877a2b6c7e55d36
won-jun Kim <glisteirng96@gmail.com>
2023-05-28 20:04:14 +0900

    initial encoding with small variance

--- src/models/tsp_model/models.py
+++ src/models/tsp_model/models.py
@@ -93,6 +93,13 @@ class Encoder(nn.Module):
         self.input_embedder = nn.Linear(2, self.embedding_dim)
         self.embedder = nn.ModuleList([EncoderLayer(**model_params) for _ in range(model_params['encoder_layer_num'])])
 
+        self.init_parameters()
+
+    def init_parameters(self):
+        for name, param in self.input_embedder.named_parameters():
+            stdv = 1. / math.sqrt(param.size(-1))
+            param.data.uniform_(-stdv, stdv)
+
     def forward(self, xy):
         out = self.input_embedder(xy)
 
