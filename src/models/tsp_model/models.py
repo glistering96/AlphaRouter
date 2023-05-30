5fa305236206b9bda8d07056ff95b92c3b573b83
won-jun Kim <glisteirng96@gmail.com>
2023-05-30 23:54:30 +0900

    Model organization fixed. Best model runs on 1028 episodes

--- src/models/tsp_model/models.py
+++ src/models/tsp_model/models.py
@@ -1,11 +1,10 @@
 import math
 
 import torch
-import torch.nn.functional as F
 from torch.distributions import Categorical
 
-from src.models.model_common import get_encoding, _to_tensor, EncoderLayer, SwiGLU
-from src.models.tsp_model.modules import *
+from src.models.model_common import get_encoding, _to_tensor, Encoder, Decoder, Value, Policy
+import torch.nn as nn
 
 
 class TSPModel(nn.Module):
@@ -16,8 +15,8 @@ class TSPModel(nn.Module):
 
         self.policy_net = Policy(**model_params)
         self.value_net = Value(**model_params)
-        self.encoder = Encoder(**model_params)
-        self.decoder = Decoder(**model_params)
+        self.encoder = Encoder(2, **model_params)
+        self.decoder = Decoder(model_params['embedding_dim'], **model_params)
 
         self.encoding = None
 
@@ -68,12 +67,12 @@ class TSPModel(nn.Module):
 
         last_node = get_encoding(self.encoding, cur_node.long(), T)
 
-        mh_atten_out = self.decoder(last_node, mask)
+        mh_attn_out = self.decoder(last_node, load=None, mask=mask)
 
-        probs = self.policy_net(mh_atten_out, self.decoder.single_head_key, mask)
+        probs = self.policy_net(mh_attn_out, self.decoder.single_head_key, mask)
         probs = probs.reshape(-1, probs.size(-1))
 
-        val = self.value_net(mh_atten_out)
+        val = self.value_net(mh_attn_out)
         val = val.reshape(-1, )
 
         return probs, val
@@ -90,70 +89,3 @@ class TSPModel(nn.Module):
         return action, None
 
 
-class Encoder(nn.Module):
-    def __init__(self, **model_params):
-        super(Encoder, self).__init__()
-
-        self.model_params = model_params
-        self.embedding_dim = model_params['embedding_dim']
-
-        self.input_embedder = nn.Linear(2, self.embedding_dim)
-        self.embedder = nn.ModuleList([EncoderLayer(**model_params) for _ in range(model_params['encoder_layer_num'])])
-
-        self.init_parameters()
-
-    def forward(self, xy):
-        out = self.input_embedder(xy)
-
-        for layer in self.embedder:
-            out = layer(out) + out
-
-        return out
-
-
-class Policy(nn.Module):
-    def __init__(self, **model_params):
-        super(Policy, self).__init__()
-        self.C = model_params['C']
-        self.embedding_dim = model_params['embedding_dim']
-
-    def forward(self, mh_attn_out, single_head_key, mask):
-        # mh_attn_out: (batch, 1, embedding_dim)
-        # single_head_key: (batch, embedding_dim, problem)
-        # mask: (batch, problem)
-
-        #  Single-Head Attention, for probability calculation
-        #######################################################
-        score = torch.matmul(mh_attn_out, single_head_key)
-        # shape: (batch, 1, problem)
-
-        sqrt_embedding_dim = math.sqrt(self.embedding_dim)
-
-        score_scaled = score / sqrt_embedding_dim
-        # shape: (batch, problem)
-
-        score_clipped = self.C * torch.tanh(score_scaled)
-
-        if score_clipped.dim() != mask.dim():
-            mask = mask.reshape(score_clipped.shape)
-
-        score_masked = score_clipped + mask
-
-        probs = F.softmax(score_masked, dim=-1)
-
-        return probs
-
-
-class Value(nn.Module):
-    def __init__(self, **model_params):
-        super(Value, self).__init__()
-        self.embedding_dim = model_params['embedding_dim']
-        self.val = nn.Sequential(
-            nn.Linear(self.embedding_dim, self.embedding_dim*2),
-            SwiGLU(),
-            nn.Linear(self.embedding_dim, 1)
-        )
-
-    def forward(self, mh_attn_out):
-        val = self.val(mh_attn_out)
-        return val
